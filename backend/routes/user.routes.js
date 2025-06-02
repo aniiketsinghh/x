5f7cb52f41dc739722291720edd33a517edd385f
aniketsingh <rajputbhaishaheb@gmail.com>
2025-06-02 23:37:49 +0530

    back

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,12 +2,12 @@ import express from "express"
 const router = express.Router();
 import {protectRoute} from "../middleware/protectRoute.middleware.js"
 
-import {getUserProfile,followUnfollowUser,getSuggestedUsers} from "../controllers/user.controller.js"
+import {getUserProfile,followUnfollowUser,getSuggestedUsers,updateUser} from "../controllers/user.controller.js"
 
 router.get("/profile/:username",protectRoute,getUserProfile)
 router.get("/suggested",protectRoute,getSuggestedUsers)
 router.post("/follow/:id",protectRoute,followUnfollowUser)
-// router.post("/update",protectRoute,updateUserProfile)
+router.post("/update",protectRoute,updateUser)
 
 
 export default router
